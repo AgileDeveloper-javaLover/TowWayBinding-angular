2552a2641d78c0e8c4c373192f8e59bca7b1df9b
AgileDeveloper-javaLover <nishantprajapati999@gmail.com>
2024-08-29 23:20:54 +0530

    Updated project

--- src/app/rect/rect.component.ts
+++ src/app/rect/rect.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
+import { Component ,effect,EventEmitter,Input,model,Output} from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-rect',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './rect.component.html',
   styleUrl: './rect.component.css',
 })
 export class RectComponent {
   // Todo: Implement custom two-way binding
 
+  // @Input({required:true}) size!: {width:string;height:string};
+  // @Output() sizeChange = new EventEmitter<{width:string;height:string}>();
+
+  size=model.required<{width:string;height:string}>();
+
+  testValue:string | undefined;
+
   onReset() {
-    // ...
+    // this.sizeChange.emit({width:'200',height:'200'});
+    this.size.set({width:'200',height:'200'});
   }
+
+
+
 }
